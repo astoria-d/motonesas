d71ba554b81f4f760b7af736a2c663f7e823d413
dmotooka <dmotooka@dti-vps-srv105.(none)>
2013-01-29 14:55:12 +0900

    bug fix...

--- src/tools.c
+++ src/tools.c
@@ -1,7 +1,8 @@
 
 #include <stdio.h>
 
-void dprint(char* s, int arg1, int arg2, int arg3, int arg4, int arg5, int arg6) {
+void dprint(char* s, char* arg1, unsigned int arg2, unsigned int arg3, 
+        unsigned int arg4, unsigned int arg5, unsigned int arg6) {
      printf(s, arg1, arg2, arg3, arg4, arg5, arg6 );
     /*
      **/
