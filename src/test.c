f2c8f2b4daae5db24570a2b391361993194b96b7
dmotooka <dmotooka@dti-vps-srv105.(none)>
2013-01-29 11:25:19 +0900

    updates.

--- src/test.c
+++ src/test.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
-int main () {
+int main (int argc, char** argv) {
+    FILE* fp;
+
     printf("main...\n");
 
-    lexmain();
+    int need_close=0;
+    if (argc > 1) {
+        fp=fopen(argv[1], "r");
+        if (fp == NULL) {
+            perror("invalid file!\n");
+            return -1;
+        }
+        need_close = 1;
+    }
+    else
+        fp=stdin;
+    lexmain(fp);
 
+    if (need_close)
+        fclose(fp);
     return 0;
 }
 
