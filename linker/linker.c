cf59cadcb09e8ae42c196b571b9806d8fcf5cf89
astoria-d <astoria-d@my-server>
2013-02-06 18:00:57 +0900

    linker load ok (temporary release). contain assembler bug, linker is not ready yet.

--- linker/linker.c
+++ linker/linker.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "tools.h"
 #include "obj-format.h"
+#include "segment.h"
+
+struct seginfo * segh2segi (struct seghdr *sgh, FILE* objfile, const char* fname) {
+    struct seginfo * pseg = malloc (sizeof(struct seginfo));
+
+    dlist_init (&pseg->list);
+    pseg->name = strdup(sgh->seg_name);
+    dprint("segment: %s\n", pseg->name);
+
+    return pseg;
+}
 
 int load_object (const char* obj_fname) {
     FILE* fp;
     struct molfhdr* molh;
     int segh_start;
-    int seg_cnt;
+    int i, seg_cnt;
 
     fp=fopen(obj_fname, "r");
     if (fp == NULL) {
@@ -23,6 +36,29 @@ int load_object (const char* obj_fname) {
     seg_cnt = molh->seg_cnt;
     segh_start = molh->segh_off;
     free(molh);
+
+
+    fseek(fp, segh_start, SEEK_SET);
+    for (i = 0; i < seg_cnt; i++) {
+        struct seghdr *sgh;
+        struct seginfo *pseg;
+
+
+        sgh = load_segh(fp);
+
+        if (!sgh) {
+            free(sgh);
+            fclose(fp);
+            return FALSE;
+        }
+
+        //pseg = segh2segi(sgh, fp, obj_fname);
+        ///must make list of seg!!!
+        //
+        //segh_start += sgh->seg_data_size;
+
+        free(sgh);
+    }
 
     return TRUE;
 }
